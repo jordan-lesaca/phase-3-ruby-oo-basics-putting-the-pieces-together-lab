9d2c080a1aec485857e4f9ed7d71db73ec354e0b
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-19 00:06:41 -0700

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def author=(author)
+        @author = author
+    end
+
+    def turn_page=(page_count)
+        @page_count = page_count
+    end
+
+    def turn_page
+        page_count
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
